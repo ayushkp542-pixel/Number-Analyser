d63cd62d349c8c8f7e42144a2a839edbfef7acf8
ayushkp542-pixel <ayushkp542@gmail.com>
2025-09-14 16:18:39 +0530

    adding basic calculator

---
+++ calculator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int x, y;
+    char choice;
+    printf("Enter the two numbers: ");
+    scanf("%d %d", &x, &y);
+    printf("Enter the Operator (+,-,*,/)\n");
+    scanf(" %c", &choice);
+    switch (choice) {
+    case '+':
+        printf("%d + %d = %d\n", x, y, x + y);
+        break;
+    case '-':
+        printf("%d - %d = %d\n", x, y, x - y);
+        break;
+    case '*':
+        printf("%d * %d = %d\n", x, y, x * y);
+        break;
+    case '/':
+        printf("%d / %d = %d\n", x, y, x / y);
+        break;
+    default:
+        printf("Invalid Operator Input\n");
+    }
+    return 0;
+}
